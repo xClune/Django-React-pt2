50a6e37886c5e6fbc42f23eb62ac7cb22ca765e1
xClune <aclune382@gmail.com>
2024-04-18 09:50:28 +0800

    Use flex display, margin-top and passed z index to make stackable.

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -10,16 +10,16 @@ function Note({note, onDelete, z}) {
     }
 
     return (
-        <div className={`note-container text-white flex flex-col w-full border-double border-4 border-blue-400 bg-blue-500 rounded-lg p-5 self-center z-${z}`}>
-            <p className="note-title">{note.title}</p>
-            <p className={`body-area ${hidden}`}>{note.body_area}</p>
-            <p className={`note-content ${hidden}`}>{note.content}</p>
-            <p className={`note-date ${hidden}`}>{ formattedDate }</p>
-            <a className="extend self-center text-white hover:cursor-pointer hover:text-blue-300" onClick={extendCard}>{
+        <div className={`note-container text-white flex flex-col w-full border-double border-4 border-blue-400 bg-blue-500 rounded-lg p-5 self-center z-${z} -mt-20`}>
+            <p className={`note-title z-${z} hover:z-50`}>{note.title}</p>
+            <p className={`body-area ${hidden} z-${z} hover:z-50`}>{note.body_area}</p>
+            <p className={`note-content ${hidden} z-${z}`}>{note.content}</p>
+            <p className={`note-date ${hidden} z-${z}`}>{ formattedDate }</p>
+            <a className={`extend self-center text-white hover:cursor-pointer hover:text-blue-300 z-${z}`} onClick={extendCard}>{
                 hidden == '' ? 'see less' : 'see more'
             }</a>
             <button 
-            className="delete-note form-button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm mt-2 w-full sm:w-auto px-3 py-1 text-center dark:bg-blue-600 dark:hover:bg-red-600 dark:focus:ring-blue-800" 
+            className={`delete-note form-button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm mt-2 w-full sm:w-auto px-3 py-1 text-center dark:bg-blue-600 dark:hover:bg-red-600 dark:focus:ring-blue-800 z-${z}`} 
             onClick={() => {
                 onDelete(note.id)
             }}>Delete
--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -26,11 +26,11 @@ function NotesList ({notes, getNotes}) {
                 {categories.map((category) => 
                 <div className='grid grid-cols-[repeat(1,minmax(150px,170px))] gap-2 self-start'
                 key={category}>
-                    <div className='p-3 self-center justify-self-center text-2xl w-full border-2 border-blue-400 bg-white-400 text-blue-400 rounded-lg'>{category}
+                    <div className='p-3 self-center justify-self-center text-2xl w-full border-2 border-blue-400 bg-white-400 text-blue-400 rounded-lg mb-20'>{category}
                     </div>
-                    <div>
+                    <div className='flex flex-col'>
                         {notes.filter((note) => note.body_area == category).map((note, index) => (
-                            <Note note={note} onDelete={deleteNote} key={note.id} z={index}/>
+                            <Note note={note} onDelete={deleteNote} key={note.id} z={index*10}/>
                         ))}
                     </div>
                 </div>
